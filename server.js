7a1f408f5c23face998455ec3e2bd4dedf5057df
alchristophergwapo <alchristopheroppa0143@gmail.com>
2021-02-22 17:48:33 +0800

    fom

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ const pusher = new Pusher({
 });
 
  //app.use('/uploads',express.static(path.join(__dirname, 'uploads')));
-var DIR = '/public/image'
+var DIR = '/uploads/image'
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, DIR);

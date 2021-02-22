3e5980e6f09edc0364b0126edf12788b68a72ac1
alchristophergwapo <alchristopheroppa0143@gmail.com>
2021-02-22 16:33:31 +0800

    uawwapiste

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(express.static(__dirname));
 //var DIR = './uploads'
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null,path.join(__dirname,'/uploads/'));
+        cb(null,path.join(__dirname,'./uploads/'));
     },
     filename: (req, file, cb) => {
         cb(null, file.originalname);
